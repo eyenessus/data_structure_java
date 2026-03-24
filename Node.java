90b6b9085f00778144652390e2b7f38dd55930bf
Emerson Sousa <eyenessus@gmail.com>
2026-03-23 23:00:50 -0300

    add: links and data on constructor

--- Node.java
+++ Node.java
@@ -1,6 +1,12 @@
 public class Node {
     public String data;
     public Node next;
+
+    public Node(String data){
+        this.data = data;
+        this.next = null;
+    }
+
     public static void main(String[] args) {
         
     }
