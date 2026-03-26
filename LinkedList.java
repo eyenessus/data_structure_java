1332151f5132abbca86a13036d2054629a92ac3d
Emerson Sousa <eyenessus@gmail.com>
2026-03-25 21:19:36 -0300

    add: method to add to tail

--- LinkedList.java
+++ LinkedList.java
@@ -1,22 +1,32 @@
 public class LinkedList {
     public Node head;
 
-    public LinkedList(){
+    public LinkedList() {
         this.head = null;
     }
-    
-    public void addToHead(String data){
+
+    public void addToHead(String data) {
         Node newHead = new Node(data);
         Node currentHead = this.head;
 
         this.head = newHead;
 
-        if(currentHead != null){
+        if (currentHead != null) {
             this.head.setNextNode(currentHead);
         }
     }
 
+    public void addToTail(String data) {
+        Node tailNode = new Node(data);
+        Node currentNode = this.head;
+
+        while (currentNode.getNextNode() != null) {
+            currentNode = currentNode.getNextNode();
+        }
+        currentNode.setNextNode(tailNode);
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
