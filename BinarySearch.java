421de83daf0dbcba80bb1917bdfe317d4ac7a534
Emerson Sousa <eyenessus@gmail.com>
2026-03-29 18:46:41 -0300

    add: added Binary Search and finding mid value of array

---
+++ BinarySearch.java
@@ -0,0 +1,11 @@
+public class BinarySearch {
+    public static int search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length;
+
+        int mid = Math.floorDiv(left + right, 2);
+
+
+        return 1;
+    }
+}
