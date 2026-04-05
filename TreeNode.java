2580e19c42155fa767466c7e328b71b625a35755
Emerson Sousa <eyenessus@gmail.com>
2026-04-05 19:49:38 -0300

    add: constructor of TreeNode

--- TreeNode.java
+++ TreeNode.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+
 public class TreeNode {
+    Object data;
+    public ArrayList<TreeNode> children;
+
+    public TreeNode(Object data) {
+        this.data = data;
+        this.children = new ArrayList<TreeNode>();
+    }
     public static void main(String[] args) {
         
     }
