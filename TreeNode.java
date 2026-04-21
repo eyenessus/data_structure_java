d14578122896ae50ec807789a74d4d7a4c141626
Emerson Sousa <eyenessus@gmail.com>
2026-04-21 16:03:37 -0300

    feat: news features to remove and add child on TreeNode

--- TreeNode.java
+++ TreeNode.java
@@ -1,14 +1,53 @@
 import java.util.ArrayList;
 
 public class TreeNode {
-    Object data;
+    public Object data;
     public ArrayList<TreeNode> children;
 
     public TreeNode(Object data) {
         this.data = data;
-        this.children = new ArrayList<TreeNode>();
+        this.children = new ArrayList<>();
     }
+
+    public void addChild(Object childData) {
+        TreeNode chilNode = new TreeNode(childData);
+        this.children.add(chilNode);
+    }
+
+    public void addChild(TreeNode childNode) {
+        this.children.add(childNode);
+    }
+
+    public void removeChild(TreeNode childNode) {
+        if(this.children.isEmpty()) {
+            return;
+        }
+
+        if(this.children.contains(childNode)) {
+            this.children.remove(childNode);
+        } else {
+            for(TreeNode node : this.children) {
+                node.removeChild(childNode);
+            }
+        }
+    }
+
+    public void removeChild(Object childData) {
+        if(this.children.isEmpty()) {
+            return;
+        }
+
+        for(TreeNode node : this.children) {
+            if(node.data.equals(childData)) {
+                this.children.remove(node);
+                return;
+            } else {
+                node.removeChild(childData);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
